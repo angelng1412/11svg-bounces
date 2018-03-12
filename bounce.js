4cf753f62e529f3604c83273d0cd4c304949502d
Tina <tchen9@stuy.edu>
2018-03-11 23:40:44 -0400

    finished

--- bounce.js
+++ bounce.js
@@ -1,25 +1,40 @@
 var svg = document.getElementById("s");
 var anim;
+var id = 0;
+var list = [];
+var xvelocity = [];
+var yvelocity = [];
+var rad = 15;
 
-var animate = function(){
-
-    var x = 40;
-    var xchange = Math.abs(Math.floor(Math.random()*10 - 5));
-    var y = 100;
-    var ychange = Math.abs(Math.floor(Math.random()*10 - 5));
-    var rad = 20;
+var circle = function(e){
     
-    var go = function(){
-	erase();
-	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-	c.setAttribute("cx", x);
-	c.setAttribute("cy", y);
-	c.setAttribute("r", rad);
-	c.setAttribute("fill", "black");
-	svg.appendChild(c);
+    clearInterval(id);
+    id = setInterval(animate, 10);  
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute("cx", e.offsetX);
+    c.setAttribute("cy", e.offsetY);
+    c.setAttribute("r", rad);
+    c.setAttribute("fill", "black");
+    svg.appendChild(c);
+    list.push(c);
 
-	x = x + xchange;
-	y = y + ychange;
+    var xchange = Math.abs(Math.floor(Math.random()*10 - 5));
+    var ychange = Math.abs(Math.floor(Math.random()*10 - 5));
+
+    xvelocity.push(xchange);
+    yvelocity.push(ychange);
+}
+
+
+
+
+var animate = function(e){
+    for (i=0; i < list.length; i++){
+	var circ = list[i];
+	var x = parseInt(circ.getAttribute("cx"));
+	var y = parseInt(circ.getAttribute("cy"));
+	var xchange = xvelocity[i];
+	var ychange = yvelocity[i];
 
 	if (x <= rad || x >= svg.width.baseVal.value - rad){
 	    xchange = -1 * xchange;
@@ -28,22 +43,29 @@ var animate = function(){
 	    ychange = -1 * ychange;
 	}
 
-	console.log(x);
-	console.log(y);
+	x = x + xchange;
+	y = y + ychange;
+
+	circ.setAttribute("cx", x);
+	circ.setAttribute("cy", y);
+	xvelocity[i] = xchange;
+	yvelocity[i] = ychange;
+	
     }
-
-    anim = setInterval(go, 10);
 }
 
-var bounce = function(e){
-    animate();
-}
 
-svg.addEventListener("click", bounce);
+
+svg.addEventListener("click", circle);
+
+var erase = function(){
+    svg.innerHTML = "";
+}
 
 var stop = function(){
     clearInterval(anim);
     anim = null;
+    list = [];
     erase();
 }
 
